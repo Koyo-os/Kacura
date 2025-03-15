59d5c20848601e1411f86d228849506cd2515b6a
osamikoyo <v.pylayev@mail.ru>
2025-03-15 16:26:15 +0500

    add worker main.go

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/koyo-os/kacura/internal/config"
+	"github.com/koyo-os/kacura/internal/handler"
+	"github.com/koyo-os/kacura/pkg/logger"
+)
+
+func main() {
+	logger := logger.Init()
+
+	cfg,err := config.Init()
+	if err != nil{
+		logger.Error(err)
+		return
+	}
+
+	handler,err := handler.Init(cfg)
+	if err != nil{
+		logger.Error(err)
+		return
+	}
+
+	http.HandleFunc("/info", handler.Counter)
+	http.HandleFunc("/", handler.MainHandler)
+
+	http.ListenAndServe(":8080", nil)
+}
