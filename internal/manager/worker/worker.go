016e7c6ccef58f56a2ee749226786325c61188a0
osamikoyo <v.pylayev@mail.ru>
2025-04-02 20:42:01 +0500

    add worker run method

--- internal/manager/worker/worker.go
+++ internal/manager/worker/worker.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	"github.com/koyo-os/kacura/internal/config"
+	"github.com/koyo-os/kacura/internal/manager/worker/agent"
 	"github.com/koyo-os/kacura/pkg/logger"
 	"github.com/wneessen/go-mail"
 )
@@ -16,10 +17,11 @@ type Worker struct{
 	ReqChan chan *http.Request
 	counterInc chan struct{}
 	counterDec chan struct{}
+	stopChan chan struct{}
 	mux *sync.Mutex
+	counter uint
+	cfg *config.Config
 }
-
-var COUNTER uint
 
 func Init(cfg *config.Config) (*Worker,error) {
 	logger := logger.Init()
@@ -41,7 +43,8 @@ func Init(cfg *config.Config) (*Worker,error) {
 	var (
 		chreq chan *http.Request
 	 	counter chan struct{}
-		counterDec chan struct{}
+	 	stopChan chan struct{}
+	 	counterDec chan struct{}
 		mux sync.Mutex
 	)
 
@@ -53,8 +56,30 @@ func Init(cfg *config.Config) (*Worker,error) {
 		ReqChan: chreq,
 		counterInc: counter,
 		counterDec: counterDec,
+		stopChan: stopChan,
 		mux: &mux,
+		cfg: cfg,
 	},nil
+}
+
+func (w *Worker) Run() error {
+	go w.CounterInc()
+	
+	for {		
+		w.counterInc <- struct{}{}
+		
+		req := <- w.ReqChan
+		agent.RunAgent(
+			&agent.Agent{
+				Wg: w.wg,
+				Logger: w.logger,
+				Client: w.client,
+				Cfg: w.cfg,
+			}
+		)
+
+		w.counterDec <- struct{}{}
+	}
 }
 
 func (w *Worker) CounterInc() {
