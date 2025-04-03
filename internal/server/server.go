1abd6d17869cf60e9402dab986791a2e5ca63d7e
osamikoyo <v.pylayev@mail.ru>
2025-04-03 06:51:13 +0500

    add server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/koyo-os/kacura/internal/config"
@@ -13,5 +15,26 @@ type Server struct {
 }
 
 func Init(cfg *config.Config) *Server {
+	return &Server{
+		server: &http.Server{
+			Addr: fmt.Sprintf("%s:%d", cfg.Host, cfg.Portv),
+		}
+		logger: logger.Init(),
+	}
+}
 
+func (s *Server) SetHandler(mux *http.ServeMux) {
+	s.server.Handler = mux
+}
+
+func (s *Server) Run() error {
+	s.logger.Infof("starting kacura server on: %s", s.server.Addr)
+
+	return s.server.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) {
+	s.logger.Info("stopping kacura server...Bye!")
+
+	s.server.Shutdown(ctx)
 }
